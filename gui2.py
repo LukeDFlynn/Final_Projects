7b762bebbc9de23cd01443f4cb24c1e80a21f4a9
LukeDFlynn <118565875+LukeDFlynn@users.noreply.github.com>
2022-12-09 20:41:42 -0600

    Project 2 Files

--- gui2.py
+++ gui2.py
@@ -2,15 +2,15 @@ from PyQt5.QtWidgets import *
 from view2 import *
 import main2
 
-excel = ""
-csv = ""
+excel = "excel"
+csv = "csv"
 
 class Controller(QMainWindow, Ui_MainWindow):
     def __init__(self, *arg, **kwargs):
         super().__init__(*arg, **kwargs)
         self.setupUi(self)
-        self.buttonExcel.clicked.connect(lambda: self.excel)
-        self.buttonCSV.clicked.connect(lambda: self.csv)
+        self.buttonExcel.clicked.connect(lambda: self.excel())
+        self.buttonCSV.clicked.connect(lambda: self.csv())
         self.radMovie.setChecked(True)
 
     def excel(self):
