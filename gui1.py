cc2f650359f9332a06abaf84d8e41797df56f507
LukeDFlynn <118565875+LukeDFlynn@users.noreply.github.com>
2022-12-09 19:56:25 -0600

    Project 1 files

--- gui1.py
+++ gui1.py
@@ -1,5 +1,5 @@
-import main
-from view import *
+import main1
+from view1 import *
 from PyQt5.QtWidgets import *
 
 class gui(QMainWindow, Ui_MainWindow):
@@ -17,6 +17,14 @@ class gui(QMainWindow, Ui_MainWindow):
             return x
         else:
             return x * self.power(x, y-1)
+
+        '''
+        This function takes in two parameters and raises the first to the second's power
+        
+        Param x: First parameter.
+        Param y: Second parameter.
+        Result: Raises x to the power of y, but also accounts for if y is 0.
+        '''
 
     def calculate(self):
         try:
@@ -37,13 +45,26 @@ class gui(QMainWindow, Ui_MainWindow):
             else:
                 self.labelAns.setText(f"Please do not make me\ncreate {ans} cat ears.")
         except:
-            self.labelAns.setText("Please only enter integers.")
+            self.labelAns.setText("Please only enter positive integers.")
+
+        '''
+        This function is called whenever the calculate button is pressed. It takes the answer given by the power
+        function and creates that many ears. Since each cat has 2 ears, it divides this number by 2 to print that
+        number of cats. If the answer given is odd, it prints "Ear" because half an ear is still missing but
+        if another cat was printed too many ears would occur.
+        
+        Result: The same number of ears as the answer given from the power function.
+        '''
 
     def clear(self):
         self.lineNum.setText("")
         self.lineExp.setText("")
         self.labelCat.setText("")
         self.labelAns.setText("")
+
+        '''
+        This function clears all text shown.
+        '''
 
 
 
--- main1.py
+++ main1.py
@@ -1,10 +1,17 @@
-from gui import *
+from gui1 import *
 
 def power(x, y):
     if y == 1:
         return x
     else:
         return x * power(x, y-1)
+    '''
+    This function takes in two parameters and raises the first value to the second value's power.
+    
+    param x: The first parameter.
+    param y: The second parameter.
+    return: x raised to the y power.
+    '''
 
 def main():
     app = QApplication([])
